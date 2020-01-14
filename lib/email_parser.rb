ca78b256424ea6436c037136b0e2821a7a920eb6
Terren Culberson <culbersonterren@yahoo.com>
2020-01-14 04:50:56 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,4 +9,10 @@ def  initialize(emails)
     @email = emails
   end
   
-  
+   def parse
+    email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
+  end
+
+end
